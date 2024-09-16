07858ea8f4803f1d2dd939cb5436e10fa8292505
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-16 11:06:21 -0700

    Completed program to find the factorial of a positive number (also provides edge case when interger entered is "negative"). The program uses for loops with main computing funtion "factorial *= i" to compute for factorial.

--- lab12/main.cpp
+++ lab12/main.cpp
@@ -1,14 +1,23 @@
-//
-//  main.cpp
-//  lab12
-//
-//  Created by Chatchapong Thuaypha on 9/16/24.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    int number;
+    long factorial = 1;
+
+    cout << "Enter a positive integer: ";
+    cin >> number;
+
+    if (number < 0) {
+        cout << "Error! You entered a negative number.";
+    }
+    else {
+        for (int i = 1; i <= number; ++i) {
+            factorial *= i;
+        }
+        cout << "Factorial of " << number << " = " << factorial << endl;
+    }
+
     return 0;
 }
